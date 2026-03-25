4777d4b01e523d29ac907f0cdbcf850b3f7fa65a
sebastian zapata montes <adrianamg1864@gmail.com>
2026-03-25 17:31:57 -0500

    Update wsgi.py

--- nuestrostore_fix/nuestrostore/wsgi.py
+++ nuestrostore_fix/nuestrostore/wsgi.py
@@ -2,7 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nuestrostore.settings')
-
-# WhiteNoise se aplica via middleware en settings.py (WHITENOISE_USE_FINDERS=True)
-# No envolvemos aquí para evitar doble registro que puede causar conflictos de rutas
 application = get_wsgi_application()
